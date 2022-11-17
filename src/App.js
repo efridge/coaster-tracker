56282380f9858fc6fa923c4df94c2c11c34c0f62
E.L. Fridge <efridge@uwf.edu>
2022-11-17 17:32:17 -0600

    Got initial DB working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CoasterForm from "./CoasterForm.js";
 import CoasterList from "./CoasterList.js";
+import { db } from "./firebase-db.js";
+import { onValue, ref, set as firebaseSet} from "firebase/database";
 
 import "./App.css";
-//import the function from the realtime database module
-//import { getDatabase, ref } from 'firebase/database';
 
 function App() {
   const [coasters, setCoasters] = useState([]);
 
-  // Get a reference to the database service
-  //const db = getDatabase();
-
-  //get reference to the "people"" property in the database
-  //const coastersRef = ref(db, "coasters")
+  useEffect(() => {
+    // Every time a new version of "coasters" comes down from the DB, update the local state
+    const query = ref(db, "coasters");
+    return onValue(query, (snapshot) => {
+      const data = snapshot.val();
+      if (snapshot.exists()) {
+          setCoasters(data);
+      }
+    });
+  }, []);
 
   const handleNewCoaster = (coasterObj) => {
-    console.log(coasterObj);
-    setCoasters([...coasters, coasterObj]);
+    //get a reference to where sarah's age is stored in the database
+    const coastersRef = ref(db, "coasters");
+
+    // Set the value in firebase. This will update our local state on the return trip from the db.
+    firebaseSet(coastersRef, [...coasters, coasterObj]);
   };
 
   return (
